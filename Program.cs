e22bf19308d195eca042c0340cb975d720f5ec50
Milan Humagain <10011027@BCS.NET.NZ>
2017-09-04 11:30:48 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace COMP5202_problem2_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nr1 = 100d;
+            double nr2 = 12d;
+            for(int i = 0; i < counter;i++){
+            Console.WriteLine("please enter a number");
+            int input = int.Parse (Console.ReadLine());
+            double result = nr1 / nr2;
+            Console.WriteLine($"{nr1} / {nr2} = {result}");
+
+          
+        }
+    }
+}
